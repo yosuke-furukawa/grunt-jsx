48b87dd5eedb3f88c535a9101930e2ded5bf2049
Yoshiki Shibukawa <yshibukawa@ngmoco.com>
2013-10-01 15:37:39 -0700

    Add 'ext' paramter and default parameter behavior

--- tasks/jsx.js
+++ tasks/jsx.js
@@ -37,6 +37,7 @@ module.exports = function(grunt) {
             test: file.test,
             args: file.args,
             output_rule : file.output_rule,
+            ext: file.ext
           },
           function(error, result, code) {
             if (!error) {
@@ -49,6 +50,23 @@ module.exports = function(grunt) {
 
   });
 
+  var changeExt = function (file, ext, opts) {
+    var output = file;
+    if (file.lastIndexOf('.') !== -1)
+    {
+      output = file.slice(0, file.lastIndexOf('.'))
+    }
+    if (ext)
+    {
+      output = output + ext;
+    }
+    else if (!opts.executable || opts.executable !== 'node')
+    {
+      output = output + '.js';
+    }
+    return output;
+  }
+
   var compileJsx = function (file, opts, callback) {
     if (!file) {
       grunt.log.warn('Source file is undefiend.');
@@ -56,12 +74,29 @@ module.exports = function(grunt) {
     }
     var args = opts.args ? opts.args.split(" ") : [];
     var output_rule = opts.output_rule;
+    var ext = opts.ext;
     delete opts.args;
     delete opts.output_rule;
+    delete opts.ext;
 
-    if (!opts.output && output_rule) {
-      //need output
-      opts.output = file.replace(output_rule.regexp, output_rule.replace);
+    if (!opts.output) {
+      if (output_rule) {
+        //need output
+        opts.output = file.replace(output_rule.regexp, output_rule.replace);
+      }
+      else
+      {
+        opts.output = changeExt(file, ext, opts);
+      }
+    }
+    else if (grunt.file.isDir(opts.output))
+    {
+      var dir = opts.output;
+      if (dir.slice(-1) !== '/')
+      {
+        dir = dir + '/';
+      }
+      opts.output = changeExt(dir + file.slice(file.lastIndexOf('/') + 1), ext, opts);
     }
 
     _.each(opts, function(value, key) {
